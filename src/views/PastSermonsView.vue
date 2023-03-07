c503644707a164176d4710a8c08ed26636303227
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-03-07 15:41:12 -0500

    Add past sermons section

--- src/views/PastSermonsView.vue
+++ src/views/PastSermonsView.vue
@@ -1,7 +1,61 @@
 <template>
-  <h1>Past sermons</h1>
-  <p>
-    We are a church with a focus on practical teaching from the Bible and applying it to our lives
-    in a friendly family like atmosphere. Everyone is welcome! We'd love to see you some day.
-  </p>
+  <div id="view-container">
+    <h1>Past sermons</h1>
+    <div class="series-container">
+      <h2>Current Series - The Law, The Covenant and The Spirit</h2>
+      <div class="sermons-container">
+        <h2>Legalism vs Antinomianism vs Epignosis in Christianity</h2>
+        <iframe
+          src="https://www.youtube-nocookie.com/embed/OX2gWbu-WUA"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+        <h2>True Anointing in the New Testament</h2>
+        <iframe
+          src="https://www.youtube-nocookie.com/embed/mtNUTuc4E14"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+        <h2>What is wrong with Old Testament Christianity</h2>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/cXWIYg7u4VM"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+        <h2>Finding Satisfaction with God</h2>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/imMgFvEUzFk"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.series-container {
+  margin-top: 1rem;
+}
+.sermons-container {
+  display: grid;
+  grid-row-gap: 1rem;
+
+  iframe {
+    width: 100%;
+    height: 20rem;
+  }
+}
+</style>
