104ba965ca6892dad89aa5d6f533d00516251a08
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-03-07 16:05:02 -0500

    Add aboutus images

--- src/views/OurMissionView.vue
+++ src/views/OurMissionView.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="view-container">
-    <h1>Our Story</h1>
-    <p>
-      Fellowship Church Streetsvile was started in 2014 when a group of men and women got together
-      who all had a shared vision of planting churches in the GTA. We are a church who believes in
-      servant leadership from all members and our desire is to make the members successful at what
-      they are doing. Our church is non-denominational in flavour, with high values in family,
-      wisdom, friendship and love. We believe our greatest calling is to develop a deep and
-      meaningful relationship with God and to understand Him in His fullness.
-    </p>
-    <p>
-      We are an open and honest church. We encourage questions. Curious on what to wear? Wear
-      whatever you are comfortable in. We are more interested in you rather than what you wear. If
-      you have a curious mind, come check us out. Pastors are available after church and if you
-      would like to meet with a pastor outside of church, you can click here to
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSfBQa9Esqt-VtiqVgkOJl7aQpYTJI3mTybBWjfE0_M94L3OIw/viewform"
-        target="_blank"
-        >request a pastoral visit.</a
-      >
-    </p>
-    <p>
-      “And he gave the apostles, the prophets, the evangelists, the shepherds and teachers, to equip
-      the saints for the work of ministry, for building up the body of Christ” – Ephesians 4:11-12
-    </p>
+    <div class="about-section">
+      <h1>Our Story</h1>
+      <div class="about-paragraphs">
+        <p>
+          Fellowship Church Streetsvile was started in 2014 when a group of men and women got
+          together who all had a shared vision of planting churches in the GTA. We are a church who
+          believes in servant leadership from all members and our desire is to make the members
+          successful at what they are doing. Our church is non-denominational in flavour, with high
+          values in family, wisdom, friendship and love. We believe our greatest calling is to
+          develop a deep and meaningful relationship with God and to understand Him in His fullness.
+        </p>
+        <p>
+          We are an open and honest church. We encourage questions. Curious on what to wear? Wear
+          whatever you are comfortable in. We are more interested in you rather than what you wear.
+          If you have a curious mind, come check us out. Pastors are available after church and if
+          you would like to meet with a pastor outside of church, you can click here to
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfBQa9Esqt-VtiqVgkOJl7aQpYTJI3mTybBWjfE0_M94L3OIw/viewform"
+            target="_blank"
+            >request a pastoral visit.</a
+          >
+        </p>
+        <p>
+          “And he gave the apostles, the prophets, the evangelists, the shepherds and teachers, to
+          equip the saints for the work of ministry, for building up the body of Christ” – Ephesians
+          4:11-12
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.about-paragraphs {
+  display: grid;
+  grid-row-gap: 1rem;
+}
+</style>
