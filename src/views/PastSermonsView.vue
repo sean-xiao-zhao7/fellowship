dc3910cf5f89e20435622332ff48f43f148a86d5
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-03-07 15:46:10 -0500

    Update past sermons intro text

--- src/views/PastSermonsView.vue
+++ src/views/PastSermonsView.vue
@@ -1,53 +1,58 @@
 <template>
   <div id="view-container">
     <h1>Past sermons</h1>
-    <div class="series-container">
-      <h2>Current Series - The Law, The Covenant and The Spirit</h2>
-      <div class="sermons-container">
-        <h2>Legalism vs Antinomianism vs Epignosis in Christianity</h2>
-        <iframe
-          src="https://www.youtube-nocookie.com/embed/OX2gWbu-WUA"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-        <h2>True Anointing in the New Testament</h2>
-        <iframe
-          src="https://www.youtube-nocookie.com/embed/mtNUTuc4E14"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-        <h2>What is wrong with Old Testament Christianity</h2>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/cXWIYg7u4VM"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-        <h2>Finding Satisfaction with God</h2>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/imMgFvEUzFk"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </div>
+    <p>
+      All sermons are on our
+      <a href="https://www.youtube.com/@fellowshipchurchrivergrove5221/videos" target="_blank"
+        >YouTube channel</a
+      >.
+    </p>
+
+    <div class="sermons-container">
+      <h2>Below is the current series - The Law, The Covenant and The Spirit.</h2>
+      <h2>Legalism vs Antinomianism vs Epignosis in Christianity</h2>
+      <iframe
+        src="https://www.youtube-nocookie.com/embed/OX2gWbu-WUA"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <h2>True Anointing in the New Testament</h2>
+      <iframe
+        src="https://www.youtube-nocookie.com/embed/mtNUTuc4E14"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <h2>What is wrong with Old Testament Christianity</h2>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/cXWIYg7u4VM"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <h2>Finding Satisfaction with God</h2>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/imMgFvEUzFk"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.series-container {
-  margin-top: 1rem;
+#view-container > p {
+  margin: 1rem 0;
 }
 .sermons-container {
   display: grid;
