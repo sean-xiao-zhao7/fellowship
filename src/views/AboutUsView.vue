545d07ab0ffa86dbec2a7bb4d07cbad130356b9e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-03-08 13:16:32 -0500

    Add pastor section

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="view-container">
     <div class="about-section">
-      <h1>Our Story</h1>
+      <h1>About Fellowship Church Streetsville</h1>
       <div class="about-paragraphs">
         <p>
-          Fellowship Church Streetsvile was started in 2014 when a group of men and women got
-          together who all had a shared vision of planting churches in the GTA. We are a church who
-          believes in servant leadership from all members and our desire is to make the members
-          successful at what they are doing. Our church is non-denominational in flavour, with high
-          values in family, wisdom, friendship and love. We believe our greatest calling is to
-          develop a deep and meaningful relationship with God and to understand Him in His fullness.
+          We started in 2014 when a group of men and women got together who all had a shared vision
+          of planting churches in the GTA. We are a church who believes in servant leadership from
+          all members and our desire is to make the members successful at what they are doing.
         </p>
+        <p>
+          Our church is non-denominational in flavour, with high values in family, wisdom,
+          friendship and love. We believe our greatest calling is to develop a deep and meaningful
+          relationship with God and to understand Him in His fullness.
+        </p>
+        <img src="../assets/images/aboutus/thursdaybiblestudy.jpg" />
         <p>
           We are an open and honest church. We encourage questions. Curious on what to wear? Wear
           whatever you are comfortable in. We are more interested in you rather than what you wear.
+        </p>
+        <p>
           If you have a curious mind, come check us out. Pastors are available after church and if
           you would like to meet with a pastor outside of church, you can click here to
           <a
@@ -29,12 +34,44 @@
         </p>
       </div>
     </div>
+    <div class="about-section">
+      <h1>Our Pastors</h1>
+      <div class="about-paragraphs">
+        <h2>Jamey Baird</h2>
+        <h3>Pastor</h3>
+        <p>
+          Jamey Baird is a graduate of Ontario Bible College and of Tyndale Theological Seminary. He
+          has been working with church planting and has done youth work for over 30 years within the
+          GTA. Jamey is married to his lovely wife Sonja, who is also involved in church planting
+          and youth work, and they are currently living in Milton. Jamey is bi-vocational, is on the
+          chaplain staff at Sheridan Villa Long Term Care facility and works with Solid Rock Chapel
+          in Clarkson.
+        </p>
+        <p>
+          Jamey loves nothing more than to answer your questions about Jesus. He understands that
+          sometimes we can get caught up with misinformation about who Jesus was, what the church is
+          or what the Bible says. He wants to help others understand more about Christ. Jamey would
+          love to meet you at a coffee shop, for lunch after church, or even in your home to answer
+          all your questions and give a clear presentation of all that Jesus came to do for us.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.about-section {
+  margin-bottom: 2rem;
+}
+
 .about-paragraphs {
   display: grid;
   grid-row-gap: 1rem;
+
+  img {
+    object-fit: cover;
+    height: 20rem;
+    width: 100%;
+  }
 }
 </style>
