65ed18fea69ea287415a23c958ae2c2996d27868
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-03-08 15:03:52 -0500

    Complete contact us view info

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,65 @@
+<script>
+import { addIcons, OhVueIcon } from 'oh-vue-icons'
+import { HiMail, BiFacebook, HiSolidLocationMarker } from 'oh-vue-icons/icons'
+addIcons(HiMail, BiFacebook, HiSolidLocationMarker)
+export default {
+  components: {
+    'v-icon': OhVueIcon
+  }
+}
+</script>
 <template>
-  <h1>Contact</h1>
-  <p>
-    We are a church with a focus on practical teaching from the Bible and applying it to our lives
-    in a friendly family like atmosphere. Everyone is welcome! We'd love to see you some day.
-  </p>
+  <div id="view-container">
+    <h1>Contact Fellowship Church Streetsville</h1>
+    <p>
+      If you love our ministry and want to know more info for an event or service please use the
+      contact info below to do so. Feel free to email either pastor and ask any question you have.
+    </p>
+    <p>You can also message us on Facebook.</p>
+    <section class="margin-section">
+      <div class="section-icon-row">
+        <v-icon class="icon" name="hi-mail" scale="2.5" fill="#4486bf" animation="wrench" hover />
+        <a href="mailto:d_trenado@hotmail.com">d_trenado[at]hotmail.com</a>
+      </div>
+      <div class="section-icon-row">
+        <v-icon class="icon" name="hi-mail" scale="2.5" fill="#4486bf" animation="wrench" hover />
+        <a
+          href="mailto:jamey.baird@gmail.com
+        "
+          >jamey.baird[at]gmail.com
+        </a>
+      </div>
+      <div class="section-icon-row">
+        <v-icon
+          class="icon"
+          name="bi-facebook"
+          scale="2.5"
+          fill="#4486bf"
+          animation="wrench"
+          hover
+        />
+        <a
+          href="https://www.facebook.com/FellowshipGTA
+        "
+          target="_blank"
+          >www.facebook.com/FellowshipGTA
+        </a>
+      </div>
+      <div class="section-icon-row">
+        <v-icon
+          class="icon"
+          name="hi-solid-location-marker"
+          scale="2.5"
+          fill="#4486bf"
+          animation="wrench"
+          hover
+        />
+        <div>
+          <p>River Grove Community Center - Bristol & Creditview.</p>
+          <p>5800 River Grove Ave. Mississauga, Ontario.</p>
+          <a href="https://goo.gl/maps/KUdqaeY1iRCyBEms9" target="_blank">See on Google Maps</a>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
