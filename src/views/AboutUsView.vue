0146d4e2233b756d0891cfe6acc6ae89008443f1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-03-08 13:20:46 -0500

    Add pastor info

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -39,6 +39,7 @@
       <div class="about-paragraphs">
         <h2>Jamey Baird</h2>
         <h3>Pastor</h3>
+        <img src="../assets/images/aboutus/jamey.png" class="portrait" />
         <p>
           Jamey Baird is a graduate of Ontario Bible College and of Tyndale Theological Seminary. He
           has been working with church planting and has done youth work for over 30 years within the
@@ -55,6 +56,34 @@
           all your questions and give a clear presentation of all that Jesus came to do for us.
         </p>
       </div>
+      <div class="about-paragraphs">
+        <h2>David Trenado</h2>
+        <h3>Pastor</h3>
+        <img src="../assets/images/aboutus/trenados.jpg" class="portrait" />
+        <p>
+          David grew up in Arganda del Rey, Madrid in Spain. From the beginning of his life he knew
+          that there was something in his inner that needed to be fulfilled. At the age of 12, he
+          surrendered his life to Christ and got baptized. From adolescence and because of his
+          parent’s example, David met the gospel in a very practical way. Helping his parents in
+          their ministry and being a witness of their love for people in social exclusion gave him a
+          perfect match of what the gospel is, a perfect example of how to live sacrificially and
+          seeking the good of others. David and his family came to Canada in April 2015, leaving
+          everything behind and without knowing anyone in Canada, taking a step of faith and
+          obedience. God was the connector between David’s family and Fellowship Church. Basketball
+          is one of David’s passions and it was because of that, Fellowship Church and David’s
+          family were connected.
+        </p>
+        <p>
+          David is responsible for helping the congregation bring people to Christ, develop
+          disciples and live lives of compassion, mercy, and justice. He preaches, teaches, works
+          with leaders and tries to meet with people as much as possible. David is married to Miriam
+          and they have 2 children, Marcos and Sofia. Marcos loves basketball & Sofia loves arts and
+          crafts. David is also serving in the ministry Upward Sports Canada as an Operations &
+          Training Director, trying to connect the community with the local church through sports
+          and the gospel. He is a basketball coach and you will find him practicing and teaching
+          basketball very often.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -67,11 +96,18 @@
 .about-paragraphs {
   display: grid;
   grid-row-gap: 1rem;
+  margin-bottom: 2rem;
 
   img {
     object-fit: cover;
     height: 20rem;
     width: 100%;
+
+    &.portrait {
+      width: 10rem;
+      height: 10rem;
+      border-radius: 10rem !important;
+    }
   }
 }
 </style>
